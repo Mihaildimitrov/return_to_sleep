ecf41b473010a70cafb534387f5bdd8f2d28ea9a
10ata <stoyan.rachev.4@gmail.com>
2021-01-17 09:42:09 +0200

    uploading MySQL Diagram

--- ServiceCare/src/Controller/HomeController.php
+++ ServiceCare/src/Controller/HomeController.php
@@ -13,14 +13,29 @@ class HomeController extends AbstractController
      /**
       * @Route("/", name="_home")
       */
-      public function Home()
+      public function Home(Request $request)
       {
+        
+        $url = $request->get('url', null);
+
+        if(!$url || !is_string($url) || ! preg_match('/^http(s)?:\/\/[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(\/.*)?$/i', $url)){
+          return new JsonResponse(['response' => null, 'error'=> 'URL not valid']);
+        }
+
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_HEADER, true);    // we want headers
+        curl_setopt($ch, CURLOPT_NOBODY, true);    // we don't need body
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+        curl_setopt($ch, CURLOPT_TIMEOUT,10);
+        $output = curl_exec($ch);
+        $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
 
         $em = $this->getDoctrine()->getManager();
         //$portfolio = $em->getRepository(PortfolioSettings::class)->findAll();
 
         
-        return new JsonResponse(['response'=>'ok']);
+        return new JsonResponse(['response'=>'HTTP code: ' . $httpcode, 'error'=> null]);
       }
 
 }
